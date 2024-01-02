fec21a3226f0d7b215aa001ca6e528eb6467eb4c
Francis Chen <tuangchok.ch@protonmail.com>
2024-01-02 09:52:45 +0700

    remove GUI client handler and papi

--- src/main/java/th/co/pixelar/lockertheft/LockerTheft.java
+++ src/main/java/th/co/pixelar/lockertheft/LockerTheft.java
@@ -1,11 +1,8 @@
 package th.co.pixelar.lockertheft;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.Server;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import th.co.pixelar.lockertheft.handler.GUITitleHandler;
 import th.co.pixelar.lockertheft.handler.LockPickingGUI;
 import th.co.pixelar.lockertheft.listeners.EventListeners;
 import th.co.pixelar.lockertheft.registries.RecipeRegistries;
@@ -15,12 +12,9 @@ public final class LockerTheft extends JavaPlugin {
     public static Server SERVER_INSTANCE;
     public static Plugin PLUGIN;
 
-    private ProtocolManager PROTOCOL_MANAGER;
-    public static GUITitleHandler GUI_TITLE_HANDLER;
-
     @Override
     public void onLoad() {
-        this.PROTOCOL_MANAGER = ProtocolLibrary.getProtocolManager();
+
     }
 
     @Override
@@ -33,8 +27,6 @@ public final class LockerTheft extends JavaPlugin {
         SERVER_INSTANCE.getPluginManager().registerEvents(new EventListeners(), this);
         SERVER_INSTANCE.getPluginManager().registerEvents(new LockPickingGUI(), this);
 
-        GUI_TITLE_HANDLER = new GUITitleHandler(this, PROTOCOL_MANAGER);
-        GUI_TITLE_HANDLER.registerPacketListeners();
 
     }
 
