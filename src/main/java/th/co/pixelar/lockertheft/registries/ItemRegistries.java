4521eee7ebbc2ac42ebd0023a91eac749260af16
Francis Chen <tuangchok.ch@protonmail.com>
2024-07-06 07:16:22 +0700

    add new items (for gui display)

--- src/main/java/th/co/pixelar/lockertheft/registries/ItemRegistries.java
+++ src/main/java/th/co/pixelar/lockertheft/registries/ItemRegistries.java
@@ -1,13 +1,10 @@
 package th.co.pixelar.lockertheft.registries;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.jetbrains.annotations.NotNull;
 import th.co.pixelar.lockertheft.utilities.ComponentManager;
 
 public class ItemRegistries {
@@ -15,6 +12,9 @@ public class ItemRegistries {
     public static ItemStack LOCK_PICKER = itemLockPicker();
     public static ItemStack KEY = itemKey();
     public static ItemStack SCREW_DRIVER = itemScrewDriver();
+    public static ItemStack NEXT_BUTTON = nextArrow();
+    public static ItemStack PREVIOUS_BUTTON = previousArrow();
+
     private static ItemStack itemLock() {
         ItemStack itemLock = new ItemStack(Material.FLINT);
         ItemMeta itemMeta = itemLock.getItemMeta();
@@ -50,6 +50,27 @@ public class ItemRegistries {
         ItemMeta itemMeta = itemKey.getItemMeta();
         itemMeta.setCustomModelData(1);
         itemMeta.displayName(Component.text("Key", ComponentManager.nonItalic(TextColor.color(255, 255, 255))));
+        itemKey.setItemMeta(itemMeta);
+
+        return itemKey;
+    }
+
+    //UI ITEMS
+    private static ItemStack nextArrow() {
+        ItemStack itemKey = new ItemStack(Material.FLINT);
+        ItemMeta itemMeta = itemKey.getItemMeta();
+        itemMeta.setCustomModelData(1001);
+        itemMeta.displayName(Component.text("Right", ComponentManager.nonItalic(TextColor.color(255, 255, 255))));
+        itemKey.setItemMeta(itemMeta);
+
+        return itemKey;
+    }
+
+    private static ItemStack previousArrow() {
+        ItemStack itemKey = new ItemStack(Material.FLINT);
+        ItemMeta itemMeta = itemKey.getItemMeta();
+        itemMeta.setCustomModelData(1002);
+        itemMeta.displayName(Component.text("Left", ComponentManager.nonItalic(TextColor.color(255, 255, 255))));
         itemKey.setItemMeta(itemMeta);
 
         return itemKey;
